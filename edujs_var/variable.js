87777895ec9b6c943e2d4ef61ba4a7724969e8dd
unknown <faisalkurnis12@gmail.com>
2023-12-10 11:24:46 +0700

    js

---
+++ edujs_var/variable.js
@@ -0,0 +1,16 @@
+// const name = "fais";
+// //name = "Eduwork";
+// console.log(name);
+
+
+// const student = {
+//     name: "Andi",
+//     class: 3
+// }
+
+// student.faculty = "Sainteks"
+// console.log(student);
+
+const students = ['Jimmy', 'Alvin', 'Pare'];
+students.push('Fais');
+console.log(students);
